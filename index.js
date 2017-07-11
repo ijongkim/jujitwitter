e6ee1ecfa1dca24a289bed481057ec5b166cca52
Innie Kim <innie.jong.kim@gmail.com>
2017-07-11 12:58:57 -0700

    Add Twitter API Key + Secret as .env variables

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const request = require('request')
 const bignum = require('bignum')
+const utils = require('./utils.js')
 const app = express()
+
+const tKey = process.env.KEY
+const tSecret = process.env.SECRET
+const tCombo = tKey + ':' + tSecret
+const tBase = new Buffer(tCombo).toString('base64')
+console.log(tCombo + '\n' + tBase)
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
