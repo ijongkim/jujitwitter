6fce28a165a5c404d6318e88160ab0f38e98d2f9
Innie Kim <innie.jong.kim@gmail.com>
2017-07-11 13:05:11 -0700

    Successfully obtain bearer token from Twitter API

--- index.js
+++ index.js
@@ -9,7 +9,33 @@ const tKey = process.env.KEY
 const tSecret = process.env.SECRET
 const tCombo = tKey + ':' + tSecret
 const tBase = new Buffer(tCombo).toString('base64')
-console.log(tCombo + '\n' + tBase)
+const token = ''
+
+// request token generated, need to request bearer token
+function getBearerToken (base, token, callback) {
+  let options = {
+    url: 'https://api.twitter.com/oauth2/token',
+    headers: {
+      'User-Agent': 'JujiTwitter',
+      'Authorization': 'Basic ' + base,
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8.'
+    },
+    body: 'grant_type=client_credentials'
+  }
+  request.post(options, function (error, response, body) {
+    if (!error) {
+      token = JSON.parse(body)
+      token = token.access_token
+      if (callback) {
+        callback(token)
+      }
+    } else {
+      console.log('Error:', error)
+    }
+  })
+}
+
+getBearerToken(tBase, token, function (output) { console.log(output) })
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
