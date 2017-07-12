b95ab10bc810cb01a15fdcf1bfdde8b9ae10b7c5
Innie Kim <innie.jong.kim@gmail.com>
2017-07-12 06:59:57 -0700

    Add various utilities to fetchRender.js, small edits to index.html

--- public/fetchRender.js
+++ public/fetchRender.js
@@ -37,10 +37,10 @@ function fetchTweets (username) {
 
 function updateBanner (username) {
   clearContainer('#sideBanner')
-  let banner = '<h1>Top 10 Representative Tweets'
+  let banner = '<h1>Top 10 Tweets'
 
   if (username) {
-    banner += ` of ${username}</h1>`
+    banner += ` Representative of ${username}</h1>`
   } else {
     banner += '</h1>'
   }
@@ -73,9 +73,9 @@ function renderUser (user) {
                         <div class="userStats"><div><h4>${parseTweet('@' + user.screen_name)}</h4>\
                         <span class="glyphicon glyphicon-user" aria-hidden="true"></span> ${parseTweet(user.description)}<br>\
                         <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> ${user.location}<br>\
-                        <span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> ${user.followers_count}<br>\
-                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span> ${user.friends_count}<br>\
-                        <span class="glyphicon glyphicon-globe" aria-hidden="true"></span> <a href="${urlInfo.expanded_url}">${urlInfo.display_url}</a><br><br>`
+                        <span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> ${addCommas(user.followers_count)}<br>\
+                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span> ${addCommas(user.friends_count)}<br>\
+                        <span class="glyphicon glyphicon-globe" aria-hidden="true"></span> <a href="${urlInfo.expanded_url}" target="_blank>${urlInfo.display_url}</a><br><br>`
 
   $user.append(userPicAndStats)
   $('#userProfile').append($user)
@@ -86,7 +86,7 @@ function renderFreq (data) {
 }
 
 function renderLoading (username) {
-  $('#displayContainer').append('<h2 class="tweetContainer panel">Analyzing @' + username + '\'s tweets...</h2>')
+  $('#displayContainer').append('<h2 class="tweetContainer panel">Analyzing @' + cleanUsername(username, 15) + '\'s tweets...</h2>')
 }
 
 function renderError (message) {
@@ -98,15 +98,34 @@ function clearContainer (selector) {
 }
 
 function parseTweet (tweet) {
-  let copy = tweet.replace(/(http:\/\/[\S]*)/ig, '<a href="$&">$&</a>')
-  copy = copy.replace(/(https:\/\/[\S]*)/ig, '<a href="$&">$&</a>')
+  let copy = tweet.replace(/(http:\/\/[\S]*)/ig, '<a href="$&" target="_blank">$&</a>')
+  copy = copy.replace(/(https:\/\/[\S]*)/ig, '<a href="$&" target="_blank">$&</a>')
   copy = copy.replace(/(#[\w]*)/ig, function (match) {
     let hashtag = match.slice(1)
-    return `<a href="https://twitter.com/hashtag/${hashtag}">#${hashtag}</a>`
+    return `<a href="https://twitter.com/hashtag/${hashtag}" target="_blank">#${hashtag}</a>`
   })
   copy = copy.replace(/(@[\w]*)/ig, function (match) {
     let mention = match.slice(1)
-    return `<a href="https://twitter.com/${mention}">@${mention}</a>`
+    return `<a href="https://twitter.com/${mention}" target="_blank">@${mention}</a>`
   })
   return copy
+}
+
+function addCommas (num) {
+  let text = num.toString()
+  let remain = text.length % 3
+  let commas = text.substr(0, remain)
+  for (var i = remain; i < text.length; i += 3) {
+    if (i === 0) {
+      commas += text.substr(i, 3)
+    } else {
+      commas += ',' + text.substr(i, 3)
+    }
+  }
+  return commas
+}
+
+function cleanUsername (username, max) {
+  let name = username.replace(/[^a-zA-Z0-9_]/g, '')
+  return name.slice(0, max)
 }
